81718a91d0dc38832f87142233864c3fa884b04b
SniperGER <6178765+SniperGER@users.noreply.github.com>
2025-05-09 22:49:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Templates
+Templates for different projects, to be used with [Bun](https://bun.sh).
+
+| | |
+|-|-|
+| [react-vite](react-vite/) | React + Vite + SWC |
+| [vanilla](vanilla/) | Vanilla template (intended for backends) |
---
+++ react-vite/vite.config.ts
@@ -0,0 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
